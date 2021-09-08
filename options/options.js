eea78a6a797538c24540c3952b80d505eba4eab6
Crowdbotics <team@crowdbotics.com>
2021-09-08 02:32:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_disk_30382Identifier",
+  url: "https://black_disk_30382Identifier.botics.co",
+  api: "https://black_disk_30382Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
